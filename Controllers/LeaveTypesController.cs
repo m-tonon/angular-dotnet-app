f13852ad9d86cde3cbc408dbd90264a77659ae49
m-tonon <math.tonon@gmail.com>
2023-06-05 07:14:11 -0300

    feat: create controller for crud operation

---
+++ Controllers/LeaveTypesController.cs
@@ -0,0 +1,150 @@
+using LeaveManagement.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Text.Encodings.Web;
+
+namespace LeaveManagement.Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LeaveTypesController : Controller
+{
+  private readonly AppDbContext _context;
+
+  public LeaveTypesController(AppDbContext context)
+  {
+    _context = context;
+  }
+
+  public async Task<IActionResult> Index()
+  {
+    var leaveTypes = await _context.LeaveTypes.ToListAsync(); // Select * from LeaveTypes
+    return Ok(leaveTypes);
+  }
+
+  // GET: LeaveTypes/Details/5
+  [HttpGet("{id}")]
+  public async Task<IActionResult> Details(int? id)
+  {
+    if (id == null)
+    {
+      return NotFound();
+    }
+
+    var leaveType = await _context.LeaveTypes
+      .FirstOrDefaultAsync(m => m.Id == id);
+    if (leaveType == null)
+    {
+      return NotFound();
+    }
+
+    return Ok(leaveType);
+  }
+
+  // GET: LeaveTypes/Create
+
+  public IActionResult Create()
+  {
+    return Ok();
+  }
+
+  // POST: LeaveTypes/Create
+  [HttpPost]
+  [ValidateAntiForgeryToken]
+
+  public async Task<IActionResult> Create([Bind("Name, DefaultDays, Id, DateCreated, DateModified")] LeaveType leaveType)
+  {
+    if (ModelState.IsValid)
+    {
+      _context.Add(leaveType);
+      await _context.SaveChangesAsync();
+      return RedirectToAction(nameof(Index));
+    }
+    return Ok(leaveType);
+  }
+
+  // GET: LeaveTypes/Edit/5
+  public async Task<IActionResult> Edit(int? id)
+  {
+    if (id == null)
+    {
+      return NotFound();
+    }
+
+    var leaveType = await _context.LeaveTypes.FindAsync(id);
+    if (leaveType == null)
+    {
+      return NotFound();
+    }
+    return Ok(leaveType);
+  }
+
+  // POST: LeaveTypes/Edit/5
+  [HttpPost]
+  [ValidateAntiForgeryToken]
+  public async Task<IActionResult> Edit(int id, [Bind("Name, DefaultDays, Id, DateCreated, DateModified")] LeaveType leaveType)
+  {
+    if (id != leaveType.Id)
+    {
+      return NotFound();
+    }
+
+    if (ModelState.IsValid)
+    {
+      try
+      {
+        _context.Update(leaveType);
+        await _context.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!LeaveTypeExists(leaveType.Id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+      return RedirectToAction(nameof(Index));
+    }
+    return Ok(leaveType);
+  }
+
+  // GET: LeaveTypes/Delete/5
+  public async Task<IActionResult> Delete(int? id)
+  {
+    if (id == null)
+    {
+      return NotFound();
+    }
+
+    var leaveType = await _context.LeaveTypes
+      .FirstOrDefaultAsync(m => m.Id == id);
+    if (leaveType == null)
+    {
+      return NotFound();
+    }
+
+    return Ok(leaveType);
+  }
+
+  // POST: LeaveTypes/Delete/5
+  [HttpPost, ActionName("Delete")]
+  [ValidateAntiForgeryToken]
+  public async Task<IActionResult> DeleteConfirmed(int id)
+  {
+    var leaveType = await _context.LeaveTypes.FindAsync(id);
+    _context.LeaveTypes.Remove(leaveType);
+    await _context.SaveChangesAsync();
+    return RedirectToAction(nameof(Index));
+  }
+
+  private bool LeaveTypeExists(int id)
+  {
+    return _context.LeaveTypes.Any(e => e.Id == id);
+  }
+
+}
